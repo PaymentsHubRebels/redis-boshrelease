6dd89f0a0d246d04a5c91a8a6b0518b683abb7ea
Panagiotis Xynos <panos@onlyascii.io>
2019-12-06 16:39:57 +0000

    introducing specs for redis and redis-sentinel jobs
    Signed-off-by: Kirubakaran Kuppusamy <kirudevops@gmail.com>

---
+++ spec/jobs/redis_spec.rb
@@ -0,0 +1,40 @@
+require 'bosh/template/test'
+
+describe 'redis job' do
+  let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '../..')) }
+  let(:job) { release.job('redis') }
+
+  let(:link) {
+
+  }
+
+  describe "redis.conf" do
+    let(:template) { job.template('config/redis.conf') }
+
+    links = [      
+      Bosh::Template::Test::Link.new(
+        name: 'redis',
+        instances: [Bosh::Template::Test::LinkInstance.new(address: '1.2.3.4', bootstrap: true)],
+        properties: {
+          'password' => 'asdf1234',
+          'port' => 4321,
+          'base_dir' => '/redis',
+        }
+      )
+    ]
+
+    it 'renders template with no errors' do
+      expect {
+        template.render({}, consumes: links)
+      }.to_not raise_error
+    end
+
+    it "chooses the bootstrap master" do
+      expect(template.render({}, consumes: links)).to match("slaveof 1.2.3.4")
+    end
+
+    it "chooses the master_host master" do
+      expect(template.render({"master_host" => "4.3.2.1"}, consumes: links)).to match("slaveof 4.3.2.1")
+    end
+  end
+end
