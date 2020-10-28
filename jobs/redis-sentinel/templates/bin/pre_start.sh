5efc73860f613faedd9e9d969225435632b9d647
Panagiotis Xynos <panos@onlyascii.io>
2020-10-28 15:10:37 +0000

    sentinel configuration needs to be persistent
    Signed-off-by: Panagiotis Xynos <panos@onlyascii.io>

--- jobs/redis-sentinel/templates/bin/pre_start.sh
+++ jobs/redis-sentinel/templates/bin/pre_start.sh
@@ -2,5 +2,5 @@
 
 set -u # report the usage of uninitialized variables
 
-chpst -u root:root mkdir -p /var/vcap/data/redis-sentinel/config
-chpst -u root:root chown -R vcap:vcap /var/vcap/data/redis-sentinel/config
+chpst -u root:root mkdir -p /var/vcap/store/redis-sentinel/config
+chpst -u root:root chown -R vcap:vcap /var/vcap/store/redis-sentinel/config
